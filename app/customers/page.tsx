e3302969ef7da597dffade341f3848d35899c653
Zaim Irfan <zaim8797@gmail.com>
2025-05-09 10:02:20 +0800

    first commit

---
+++ app/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Customer() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-4">
+            <h1 className="text-2xl font-bold">Customers</h1>
+            <p className="mt-4 text-gray-600">Welcome to the customers page!</p>
+        </div>
+    );
+}
---
+++ app/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Invoice() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-4">
+            <h1 className="text-2xl font-bold">Invoices</h1>
+            <p className="mt-4 text-gray-600">Welcome to the invoices page!</p>
+        </div>
+    );
+}
